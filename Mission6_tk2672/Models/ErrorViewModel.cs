b4ab751d7f745d0d9f7b3e54eced7b5e2a074de9
TylerK2672 <tylerchristiankendall@gmail.com>
2023-02-08 13:28:37 -0700

    first commit

---
+++ Mission6_tk2672/Models/ErrorViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mission6_tk2672.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
+
